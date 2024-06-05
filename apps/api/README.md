b7734b201cdcd010a27d22d6095d968ae1d294c7
henri <henri.me@skiff.com>
2024-06-05 21:16:53 +0200

    added tailwind and shadcn

--- apps/api/README.md
+++ apps/api/README.md
@@ -1,1 +1,4 @@
-# `api`
+<p align="center">
+    <h1 align="center">box - backend</h1>
+    <p align="center">Our backend is built with hono, a fast express alternativ.</p>
+</p>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center">
+      <div className="w-full mx-5 md:w-[600px] lg:w-[800px] bg-[#e1e1e1] dark:bg-[#171717] text-[#e1e1e1]">ghey</div>
     </div>
   )
 }
