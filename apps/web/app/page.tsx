2529ec4a3a2c5b0924ce6ca4c1c878c38aba6e09
henri <henri.me@skiff.com>
2024-06-05 21:58:44 +0200

    added t3 starter and deleted the 2 old examples

--- apps/web/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center">
-      <div className="w-full mx-5 md:w-[600px] lg:w-[800px] bg-[#e1e1e1] dark:bg-[#171717] text-[#e1e1e1]">ghey</div>
-    </div>
-  )
-}
