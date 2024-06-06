f2632df1b67bf2a66e4e3b0930db904caadb1e23
henri <henri.me@skiff.com>
2024-06-06 17:52:21 +0200

    web: added a new homepage example and init shadcn-ui for the components

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import Navigation from "~/components/Navigation/nav";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center bg-[#e1e1e1] dark:bg-[#171717] min-h-screen">
-      <h2 className="text-4xl text-[#343434] dark:text-[#e1e1e1]">
-        under constructions...
-      </h2>
+    <div className="flex flex-col items-center w-full bg-neutral-50  text-neutral-700 dark:text-neutral-300 dark:bg-neutral-950 min-h-screen">
+      <div className="w-full mx-5 md:mx-5 md:w-[600px] lg:w-[700px]">
+        <Navigation />
+        <h2 className="text-4xl text-neutral-700 dark:text-neutral-300">
+          box
+        </h2>
+      </div>
     </div>
   )
 }
---
+++ apps/web/src/components/Navigation/nav.tsx
@@ -0,0 +1,19 @@
+import DarkModeSvg from "public/box-light.svg"
+import LightModeSvg from "public/box.svg"
+import Img from "next/image"
+import {Button} from "@repo/ui/button"
+import Link from "next/link"
+export default function Navigation() {
+    return (
+        <header className="flex items-center justify-between text-neutral-900 dark:text-neutral-100">
+            <Link href="/">
+                <Img src={DarkModeSvg} height={30} width={30} alt="The box logo." />
+            </Link>
+            <nav>
+                <Link href="/features">features</Link>
+                <Link href="/pricing">pricing</Link>
+                <Button appName="hey">hey</Button>
+            </nav>
+        </header>
+    )
+}
